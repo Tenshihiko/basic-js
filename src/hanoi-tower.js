09f6646a26d9a307c682fe53631ad521098fc5ca
Tenshihiko Arai <arai.tenshi@gmail.com>
2020-06-28 03:07:38 +0400

    refactor: hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,10 +2,9 @@ const CustomError = require('../extensions/custom-error');
 
 const SEC_IN_HOUR = 3600;
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  const
-    turns = Math.pow(2, disksNumber) - 1,
-    turnsAtSec = turnsSpeed / SEC_IN_HOUR,
-    seconds = Math.floor(turns / turnsAtSec);
+  const turns = 2 ** disksNumber - 1;
+  const turnsAtSec = turnsSpeed / SEC_IN_HOUR;
+  const seconds = Math.floor(turns / turnsAtSec);
 
   return {
     turns,
