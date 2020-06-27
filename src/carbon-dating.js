2a3575adb1750c573d803316b050a1fc6a9ac40f
Tenshihiko Arai <arai.tenshi@gmail.com>
2020-06-27 23:34:12 +0400

    feat: solve carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const LN2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const activityNum = parseFloat(sampleActivity);
+  if (typeof (sampleActivity) !== 'string'
+    || !sampleActivity
+    || Number.isNaN(activityNum)
+    || activityNum <= 0
+    || activityNum >= MODERN_ACTIVITY) {
+    return false;
+  }
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(activityNum)) / (LN2 / HALF_LIFE_PERIOD));
 };
