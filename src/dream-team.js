59b0f49cb54a9fa8e235e7404e5b0ea5786f1dd2
Tenshihiko Arai <arai.tenshi@gmail.com>
2020-06-27 23:46:50 +0400

    feat: solve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let teamName = '';
+  members.forEach((element) => {
+    if (typeof (element) === 'string' && element.length > 0) {
+      teamName += element.trim().toUpperCase()[0];
+    }
+  });
+
+  return teamName.split('').sort().join('');
 };
