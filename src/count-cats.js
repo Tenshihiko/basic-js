4c25f473b7f7777a050cd12cab15425e0531e2f8
Tenshihiko Arai <arai.tenshi@gmail.com>
2020-06-27 23:03:44 +0400

    refactor: add eslint, refactor count cats to pass eslint

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,14 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 module.exports = function countCats(backyard) {
   let count = 0;
-  backyard.forEach(line => {
-    line.forEach(cell => {
-      if (cell == '^^') count++;
-    })
+
+  backyard.forEach((line) => {
+    line.forEach((cell) => {
+      if (cell === '^^') {
+        count++;
+      }
+    });
   });
   return count;
 };
