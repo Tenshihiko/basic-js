710b135bf12c624d9799a010f699a58708e78eb9
Tenshihiko Arai <arai.tenshi@gmail.com>
2020-06-28 03:58:21 +0400

    feat: solve extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function getValueOrDefault(obj, key, def) {
+  return key in obj ? obj[key] || def : def;
+}
+
+function oneLvlRepeater(repeated, options) {
+  const str = repeated === null ? 'null'
+    : repeated === undefined ? ''
+      : repeated.toString();
+
+  const times = getValueOrDefault(options, 'repeatTimes', 1);
+  const separator = getValueOrDefault(options, 'separator', '');
+  const addition = getValueOrDefault(options, 'addition', '');
+
+  return new Array(times).fill(str).map((e) => e + addition).join(separator);
+}
+
+module.exports = function repeater(str, options) {
+  const addition = oneLvlRepeater(options.addition, {
+    repeatTimes: getValueOrDefault(options, 'additionRepeatTimes', 0),
+    addition: null,
+    separator: getValueOrDefault(options, 'additionSeparator', '|'),
+  });
+
+  return oneLvlRepeater(str, {
+    repeatTimes: options.repeatTimes,
+    addition,
+    separator: getValueOrDefault(options, 'separator', '+'),
+  });
 };
-  
